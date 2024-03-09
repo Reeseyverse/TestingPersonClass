5f9d7810b98bceb38ad2bc12986d69295bd82c37
Reeseyverse <jorrisecko@gmail.com>
2024-03-08 20:39:32 -0500

    finished

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -3,33 +3,86 @@ package com.zipcodewilmington.person;
 /**
  * If you did ObjectsOne lab, you might recognize this code?
  */
-public class Person {
+public class Person { // CLASS
     private String name;
     private int age;
 
-    public Person() {
-    }
+    private boolean job;
 
-    public Person(int age) {
-    }
+    private String partnerName;
 
-    public Person(String name) {
-    }
+    private boolean pets;
 
-    public Person(String name, int age) {
-    }
+    private int partnerAge;
+
+    private int petsAge;
+
+    //CONSTRUCTOR
+
+    // Getters and Setters
+
 
     public void setName(String name) {
+        this.name = "Leon";
     }
 
     public void setAge(int age) {
+        this.age = 5;
     }
 
+
+    // MINE
+    public void setPartnerName(String expected) {
+        this.partnerName = "Barbara";
+    }
+
+    public void setPets(boolean expected) {
+        this.pets = true;
+    }
+
+    public void setJob(boolean expected) {
+        this.job = false;
+    }
+
+    public void setPartnerAge(int expected) {
+        this.partnerAge = 26;
+    }
+
+    public void setPetsAge(int expected) {
+        this.petsAge = 2;
+    }
+// END OF MINE
+
+
     public String getName() {
-        return null;
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
     }
+
+    // MINE
+    public String getPartnerName() {
+        return partnerName;
+    }
+
+    public boolean isPets() {
+        return pets;
+    }
+
+    public boolean isJob() {
+        return job;
+    }
+
+    public int getPartnerAge() {
+        return partnerAge;
+    }
+
+    public int getPetsAge() {
+        return petsAge;
+    }
+    // MINE ENDS
+
 }
+
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -7,66 +7,7 @@ import org.junit.Test;
  * Created by leon on 2/12/18.
  */
 public class TestPerson {
-    @Test
-    public void testDefaultConstructor() {
-        // Given
-        String expectedName = "";
-        Integer expectedAge = Integer.MAX_VALUE;
 
-        // When
-        Person person = new Person();
-
-        // Then
-        String actualName = person.getName();
-        Integer actualAge = person.getAge();
-
-        Assert.assertEquals(expectedName, actualName);
-        Assert.assertEquals(expectedAge, actualAge);
-    }
-
-    @Test
-    public void testConstructorWithName() {
-        // Given
-        String expected = "Leon";
-
-        // When
-        Person person = new Person(expected);
-
-        // Then
-        String actual = person.getName();
-        Assert.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testConstructorWithAge() {
-        // Given
-        Integer expected = 5;
-
-        // When
-        Person person = new Person(expected);
-
-        // Then
-        Integer actual = person.getAge();
-        Assert.assertEquals(expected, actual);
-    }
-
-
-    @Test
-    public void testConstructorWithNameAndAge() {
-        // Given
-        Integer expectedAge = 5;
-        String expectedName = "Leon";
-
-        // When
-        Person person = new Person(expectedName, expectedAge);
-
-        // Then
-        Integer actualAge = person.getAge();
-        String actualName = person.getName();
-
-        Assert.assertEquals(expectedAge, actualAge);
-        Assert.assertEquals(expectedName, actualName);
-    }
 
     @Test
     public void testSetName() {
@@ -95,4 +36,65 @@ public class TestPerson {
         Integer actual = person.getAge();
         Assert.assertEquals(expected, actual);
     }
+    //MINE
+    @Test
+    public void testPartnerName(){
+        // Given
+        Person person = new Person();
+        String expected = "Barbara";
+
+        // When
+        person.setPartnerName(expected);
+        String actual = person.getPartnerName();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testPets(){
+        Person person = new Person();
+        boolean expected = true;
+
+        person.setPets(expected);
+        boolean actual = person.isPets();
+
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testJob(){
+        Person person = new Person();
+        boolean expected = false;
+
+        person.setJob(expected);
+        boolean actual = person.isJob();
+
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testPetsAge() {
+        // Given
+        Person person = new Person();
+        Integer expected = 2;
+
+        // When
+        person.setPetsAge(expected);
+
+        // Then
+        Integer actual = person.getPetsAge();
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testPartnersAge() {
+        // Given
+        Person person = new Person();
+        Integer expected = 26;
+
+        // When
+        person.setPartnerAge(expected);
+
+        // Then
+        Integer actual = person.getPartnerAge();
+        Assert.assertEquals(expected, actual);
+    }
 }
+
